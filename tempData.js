16197e5da7e0ce10a36e41e1e9a869aac5dceced
Doumani <samuel.doumani20@inphb.ci>
2023-01-15 17:38:36 +0000

    projetodolist

---
+++ tempData.js
@@ -0,0 +1,73 @@
+
+
+export default tempData = [
+    {
+        id: 1,
+        name: "Planifier un Voyage",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Reserver un Vol",
+                completed: false
+            },
+            {
+                title: "Contrôle des Passeports",
+                completed: true
+            },
+            {
+                title: "Reserver une Chambre d'Hôtel",
+                completed: true
+            },
+            {
+                title: "Pack Bagages",
+                completed: false
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Courses",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Acheter du Lait",
+                completed: false
+            },
+            {
+                title: "Planifier un Week-end de Camping",
+                completed: true
+            },
+            {
+                title: "Courir 2 miles",
+                completed: true
+            },
+            {
+                title: "Créer une vidéo",
+                completed: true
+            },
+            {
+                title: "Promenez votre chien",
+                completed: true
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Anniversaire",
+        color: "#595BD9",
+        todos: [
+            {
+                title: "Obtenir des ballons",
+                completed: false
+            },
+            {
+                title: "Envoyer des Invitations",
+                completed: true
+            },
+            {
+                title: "Faire des reservations pour le Dîner",
+                completed: true
+            }
+        ]
+    }
+];
